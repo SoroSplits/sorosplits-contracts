bb36280dc145608e6f61c74c64c858653e670b62
findolor <anakisci@gmail.com>
2023-08-31 15:46:23 +0300

    Add trait for contract operations

--- contracts/splitter/src/contract.rs
+++ contracts/splitter/src/contract.rs
@@ -1,9 +1,15 @@
-use soroban_sdk::{contract, contractimpl};
+use soroban_sdk::{contract, contractimpl, Env, Vec};
+
+use crate::storage::ShareDataKey;
+
+pub trait SplitterTrait {
+    fn init(env: Env, shares: Vec<ShareDataKey>);
+
+    fn distribute_tokens();
+}
 
 #[contract]
 pub struct Splitter;
 
 #[contractimpl]
-impl Splitter {
-
-}
+impl Splitter {}
