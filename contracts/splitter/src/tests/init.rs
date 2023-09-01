020f0045b0efb1a320c0d898ba422d3f6d198c3f
findolor <anakisci@gmail.com>
2023-09-01 09:49:29 +0300

    Add simple test for init operation

---
+++ contracts/splitter/src/tests/init.rs
@@ -0,0 +1,23 @@
+use soroban_sdk::{testutils::Address as _, vec, Address, Env};
+
+use crate::{storage::ShareDataKey, tests::helpers::create_splitter};
+
+#[test]
+fn happy_path() {
+    let env = Env::default();
+    let splitter = create_splitter(&env);
+
+    let shares = vec![
+        &env,
+        ShareDataKey {
+            shareholder: Address::random(&env),
+            share: 8050,
+        },
+        ShareDataKey {
+            shareholder: Address::random(&env),
+            share: 1950,
+        },
+    ];
+
+    splitter.init(&shares)
+}
