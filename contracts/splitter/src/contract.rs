808708959eda832fce222f44395162052d6cb58d
findolor <anakisci@gmail.com>
2023-09-01 08:08:11 +0300

    Implement trait and add init operation

--- contracts/splitter/src/contract.rs
+++ contracts/splitter/src/contract.rs
@@ -5,11 +5,21 @@ use crate::storage::ShareDataKey;
 pub trait SplitterTrait {
     fn init(env: Env, shares: Vec<ShareDataKey>);
 
-    fn distribute_tokens();
+    fn distribute_tokens(env: Env);
 }
 
 #[contract]
 pub struct Splitter;
 
 #[contractimpl]
-impl Splitter {}
+impl SplitterTrait for Splitter {
+    fn init(env: Env, shares: Vec<ShareDataKey>) {
+        for share in shares.iter() {
+            env.storage()
+                .persistent()
+                .set(&share.recipient, &share.share);
+        }
+    }
+
+    fn distribute_tokens(env: Env) {}
+}
