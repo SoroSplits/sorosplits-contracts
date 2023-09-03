31d44745efd4487199dede2a9429eab59dd1c14c
findolor <anakisci@gmail.com>
2023-09-03 15:53:59 +0300

    Add helper for splitter

--- contracts/splitter/src/tests/helpers.rs
+++ contracts/splitter/src/tests/helpers.rs
@@ -1,11 +1,18 @@
-use crate::contract::{Splitter, SplitterClient};
+use soroban_sdk::{token, Address, Env, Vec};
+use token::{AdminClient as TokenAdminClient, Client as TokenClient};
 
-use soroban_sdk::{token, Address, Env};
-use token::AdminClient as TokenAdminClient;
-use token::Client as TokenClient;
+use crate::{
+    contract::{Splitter, SplitterClient},
+    storage::ShareDataKey,
+};
 
 pub fn create_splitter(e: &Env) -> SplitterClient {
     SplitterClient::new(&e, &e.register_contract(None, Splitter))
+}
+
+pub fn create_splitter_with_shares(e: &Env, shares: &Vec<ShareDataKey>) {
+    let client = create_splitter(e);
+    client.init(shares)
 }
 
 pub fn create_token<'a>(e: &Env, admin: &Address) -> (TokenClient<'a>, TokenAdminClient<'a>) {
