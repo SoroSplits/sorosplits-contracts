44866acd813befb4a027ce5dc57469187999bb9c
findolor <anakisci@gmail.com>
2023-10-17 18:46:13 +0300

    Add errors

---
+++ contracts/splitter/src/errors.rs
@@ -0,0 +1,14 @@
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    NotInitialized = 1,
+    AlreadyInitialized = 2,
+    Unauthorized = 3,
+    ContractLocked = 4,
+    LowShareCount = 5,
+    ShareLimitReached = 6,
+    ShareLimitNotReached = 7,
+}
