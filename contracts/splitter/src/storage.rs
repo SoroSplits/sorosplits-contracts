dee50e36681eee229ed24facf062941fb0941166
findolor <anakisci@gmail.com>
2023-11-02 09:30:42 +0300

    Add lifetime bump support to storage values

--- contracts/splitter/src/storage.rs
+++ contracts/splitter/src/storage.rs
@@ -1,6 +1,29 @@
-use soroban_sdk::{contracttype, Address, Env, Vec};
+use soroban_sdk::{contracttype, Address, Env, IntoVal, Val, Vec};
 
 use crate::errors::Error;
+
+const DAY_IN_LEDGERS: u32 = 17280;
+
+const INSTANCE_BUMP_AMOUNT: u32 = 7 * DAY_IN_LEDGERS;
+const INSTANCE_LIFETIME_THRESHOLD: u32 = INSTANCE_BUMP_AMOUNT - DAY_IN_LEDGERS;
+
+const PERSISTENT_BUMP_AMOUNT: u32 = 30 * DAY_IN_LEDGERS;
+const PERSISTENT_LIFETIME_THRESHOLD: u32 = PERSISTENT_BUMP_AMOUNT - DAY_IN_LEDGERS;
+
+fn bump_instance(e: &Env) {
+    e.storage()
+        .instance()
+        .bump(INSTANCE_LIFETIME_THRESHOLD, INSTANCE_BUMP_AMOUNT);
+}
+
+fn bump_persistent<K>(e: &Env, key: &K)
+where
+    K: IntoVal<Env, Val>,
+{
+    e.storage()
+        .persistent()
+        .bump(key, PERSISTENT_LIFETIME_THRESHOLD, PERSISTENT_BUMP_AMOUNT);
+}
 
 #[derive(Clone, Debug, PartialEq)]
 #[contracttype]
@@ -15,12 +38,20 @@ impl ShareDataKey {
         e.storage()
             .persistent()
             .set(&key, &ShareDataKey { shareholder, share });
+        bump_persistent(e, &key);
     }
 
     /// Returns the share for the shareholder
     pub fn get_share(e: &Env, shareholder: &Address) -> Option<ShareDataKey> {
         let key = DataKey::Share(shareholder.clone());
-        e.storage().persistent().get(&key)
+        let res = e.storage().persistent().get::<DataKey, ShareDataKey>(&key);
+        match res {
+            Some(share) => {
+                bump_persistent(e, &key);
+                Some(share)
+            }
+            None => None,
+        }
     }
 
     /// Removes the share for the shareholder
@@ -31,22 +62,28 @@ impl ShareDataKey {
 
     /// Saves the list of shareholders
     pub fn save_shareholders(e: &Env, shareholders: Vec<Address>) {
-        e.storage()
-            .persistent()
-            .set(&DataKey::Shareholders, &shareholders);
+        let key = DataKey::Shareholders;
+        e.storage().persistent().set(&key, &shareholders);
+        bump_persistent(e, &key);
     }
 
     /// Returns the list of shareholders
     pub fn get_shareholders(e: &Env) -> Vec<Address> {
-        e.storage()
-            .persistent()
-            .get::<DataKey, Vec<Address>>(&DataKey::Shareholders)
-            .unwrap_or(Vec::new(&e))
+        let key = DataKey::Shareholders;
+        let res = e.storage().persistent().get::<DataKey, Vec<Address>>(&key);
+        match res {
+            Some(shareholders) => {
+                bump_persistent(e, &key);
+                shareholders
+            }
+            None => Vec::new(&e),
+        }
     }
 
     /// Removes the list of shareholders
     pub fn remove_shareholders(e: &Env) {
-        e.storage().persistent().remove(&DataKey::Shareholders);
+        let key = DataKey::Shareholders;
+        e.storage().persistent().remove(&key);
     }
 }
 
@@ -59,6 +96,7 @@ pub struct ConfigDataKey {
 impl ConfigDataKey {
     /// Initializes the config with the given admin address and mutable flag
     pub fn init(e: &Env, admin: Address, mutable: bool) {
+        bump_instance(e);
         let key = DataKey::Config;
         let config = ConfigDataKey { admin, mutable };
         e.storage().instance().set(&key, &config);
@@ -66,12 +104,14 @@ impl ConfigDataKey {
 
     /// Returns the config
     pub fn get(e: &Env) -> Option<ConfigDataKey> {
+        bump_instance(e);
         let key = DataKey::Config;
         e.storage().instance().get(&key)
     }
 
     /// Locks the contract for further changes
     pub fn lock_contract(e: &Env) {
+        bump_instance(e);
         let key = DataKey::Config;
         let config: Option<ConfigDataKey> = e.storage().instance().get(&key);
         match config {
@@ -85,12 +125,14 @@ impl ConfigDataKey {
 
     /// Returns true if ConfigDataKey exists in the storage
     pub fn exists(e: &Env) -> bool {
+        bump_instance(e);
         let key = DataKey::Config;
         e.storage().instance().has(&key)
     }
 
     /// Validates the admin address
     pub fn require_admin(e: &Env) -> Result<(), Error> {
+        bump_instance(e);
         let key = DataKey::Config;
         let config: ConfigDataKey = e.storage().instance().get(&key).unwrap();
         config.admin.require_auth();
@@ -100,6 +142,7 @@ impl ConfigDataKey {
     /// Returns true if the contract is mutable
     // TODO: Maybe return an error if ConfigDataKey doesn't exist
     pub fn is_contract_locked(e: &Env) -> bool {
+        bump_instance(e);
         let key = DataKey::Config;
         let config: Option<ConfigDataKey> = e.storage().instance().get(&key);
         match config {
